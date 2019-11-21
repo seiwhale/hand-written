795a25ac54a171d8c479807e9b0066e93a84192f
鲸落 <32949884+seiwhale@users.noreply.github.com>
2019-11-21 23:28:10 +0800

    :sparkles: 新增 push & pop

---
+++ 16 手写 push & pop.js
@@ -0,0 +1,10 @@
+Array.prototype.myPush = function () {
+  for (var i = 0; i< arguments.length; i++) {
+    this[this.length] = arguments[i]
+  }
+  return this.length
+}
+
+Array.prototype.myPop = function () {
+  return this.splice(this.length - 1, 1)[0];
+}
