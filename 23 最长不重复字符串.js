71f7a502565a0777525548481f050520656353a7
李世杰 <lishijie@shouqianba.com>
2021-09-29 23:21:53 +0800

    feat: 更新 找出不含有重复字符的 最长子串

--- 23 最长不重复字符串.js
+++ 23 最长不重复字符串.js
@@ -28,3 +28,23 @@ var lengthOfLongestSubstring = function (s) {
   }
   return ans;
 };
+
+
+/**
+ * 优化：返回最长重复字符串的 长度、字符、起始、截止下标
+ * @param {*} str 
+ */
+const findMaxLengthString = (str) => {
+  const occ = new Map()
+  let data, occ_key = 'max'
+
+  for (let i = 0, j = 1; i < str.length; j++) {
+    if (str[i] !== str[j]) {
+      if (occ.has(occ_key)) data = occ.get(occ_key)
+      if (!data || data.count < j - i) occ.set(occ_key, { start: i, end: j, char: str[i], count: j - i })
+      i = j
+    }
+  }
+
+  return occ.get(occ_key)
+}
